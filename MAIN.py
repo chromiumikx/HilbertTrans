5917e56f6e58f0cb5b507d6c6eb88823b66588dc
Chromiumikx <studyfori@126.com>
2016-05-26 16:19:26 +0800

    增加文件路径操作，增加各种调制方式的时序幅值数据集

--- MAIN.py
+++ MAIN.py
@@ -1,2 +1,19 @@
+import os
+
 def hilbert(x):
     pass
+    return y
+
+
+def testFileKeep():
+    NowPath = os.getcwd()
+    L = os.listdir(NowPath+"\\TrainingData")
+    print L
+    os.chdir(NowPath+"\\TrainingData")
+    f = open(L[0])
+    a = f.readlines()
+    print(a)
+
+
+if __name__ == "__main__":
+    testFileKeep()
