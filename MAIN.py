56dd8c076dfba63d936d8432e625dc4b9308b5fd
Chromiumikx <studyfori@126.com>
2016-08-18 23:25:44 +0800

    新建

--- MAIN.py
+++ MAIN.py
@@ -1,13 +1,20 @@
-import os
+﻿import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy import fftpack
 
 def hilbert(x):
-    pass
-    return y
+    t = np.linspace(1e-10, 2*np.pi, len(x))
+    y = np.convolve(x,1/(np.pi*t))
+    return y[0:len(x)]/1e9
 
 
+##测试从路径打开文件
 def testFileKeep():
     NowPath = os.getcwd()
     L = os.listdir(NowPath+"\\TrainingData")
+    ##列出该文件夹下的文件的名字
     print L
     os.chdir(NowPath+"\\TrainingData")
     f = open(L[0])
@@ -16,4 +23,17 @@ def testFileKeep():
 
 
 if __name__ == "__main__":
-    testFileKeep()
+    #testFileKeep()
+
+    SamplingRate = 100.0
+    t = np.linspace(1e-10, 2*np.pi, SamplingRate)
+    x = np.cos(t)
+    hy = hilbert(x)
+
+    hhy = fftpack.hilbert(x)
+
+    print(len(t), len(x))
+
+    plt.plot(t,x)
+    plt.plot(t,hhy)
+    plt.show()
